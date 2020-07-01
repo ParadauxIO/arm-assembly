4f23edadfa75d6364d1e15eec89c1cfdb5469150
Jonathan Dukes <jdukes@surprise.fritz.box>
2020-07-01 22:32:03 +0100

    ex1-1-add: Fix error

--- ex-1-1-add/add.s
+++ ex-1-1-add/add.s
@@ -14,7 +14,7 @@ Main:
   
   @ Write a program to compute total=a+b+c+d
   MOV   R0, R1          @ total=a
-  ADD   R0, R0, R1      @ total=total+b
+  ADD   R0, R0, R2      @ total=total+b
   ADD   R0, R0, R3      @ total=total+c
   ADD   R0, R0, R4      @ total=total+d
 
