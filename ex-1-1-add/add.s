f3c7bd4a261c41acfa01bee4c9c083e3b3bb533c
Jonathan Dukes <jdukes@surprise.fritz.box>
2020-07-02 11:17:55 +0100

    ex-1-1-add: move test initialisation to test.s

--- ex-1-1-add/add.s
+++ ex-1-1-add/add.s
@@ -2,16 +2,11 @@
   .cpu cortex-m4
   .fpu softvfp
   .thumb
+
   .global  Main
 
 Main:
 
-  @ First, set up a, b, c and d with some test values
-  MOV   R1, #6          @ a=6
-  MOV   R2, #7          @ b=7
-  MOV   R3, #8          @ c=8
-  MOV   R4, #9          @ d=9
-  
   @ Write a program to compute total=a+b+c+d
   MOV   R0, R1          @ total=a
   ADD   R0, R0, R2      @ total=total+b
@@ -22,4 +17,5 @@ Main:
 
 End_Main:
   BX    lr
-.size Main, .-Main
+
+  .end
