8faa219d2649fab1978fab399890d4b18189e260
Jonathan Dukes <jdukes@surprise.fritz.box>
2020-07-10 21:58:17 +0100

    ex-1-1-add: change test

--- ex-1-1-add/test.s
+++ ex-1-1-add/test.s
@@ -10,10 +10,10 @@
   .type     Init_Test, %function
 Init_Test:
   @ Set up a, b, c and d with some test values
-  MOV   R1, #6          @ a=6
-  MOV   R2, #7          @ b=7
-  MOV   R3, #8          @ c=8
-  MOV   R4, #9          @ d=9
+  MOV   R1, #2          @ a=6
+  MOV   R2, #3          @ b=7
+  MOV   R3, #4          @ c=8
+  MOV   R4, #5          @ d=9
   bx    lr
 
 .end
