b99109e227b07babb12652e281707ff77b577bdf
Jonathan Dukes <jdukes@MacBook-Pro.local>
2020-09-18 13:32:55 +0100

    ex-2-1-swap: add exercise

---
+++ ex-2-1-swap/swap.s
@@ -0,0 +1,15 @@
+  .syntax unified
+  .cpu cortex-m4
+  .thumb
+  .global  Main
+
+Main:
+  
+  @ Write a program to swap the contents of R0 and R1
+
+  @ *** your solution goes here ***
+
+  @ End of program ... check your result
+
+End_Main:
+  BX    lr
