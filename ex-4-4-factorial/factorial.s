256bb62007ad1369225b196311cab95269d227c8
Jonathan Dukes <jdukes@MacBook-Pro.fritz.box>
2020-09-29 16:59:59 +0100

    ex-4-4-factorial: add exercise

---
+++ ex-4-4-factorial/factorial.s
@@ -0,0 +1,17 @@
+  .syntax unified
+  .cpu cortex-m4
+  .thumb
+  .global  Main
+
+Main:
+
+  @ Calculate n!, where n is stored in R1
+
+
+
+  @ End of program ... check your result
+
+End_Main:
+  BX    lr
+
+.end
