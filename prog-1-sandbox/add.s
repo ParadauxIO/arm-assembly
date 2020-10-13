dbe74f114cd83a25199d0a96974c83aa14fc9251
Jonathan Dukes <jdukes@tcd.ie>
2020-10-13 09:45:14 +0100

    prog-1-sandbox: create template program

---
+++ prog-1-sandbox/add.s
@@ -0,0 +1,17 @@
+  .syntax unified
+  .cpu cortex-m4
+  .thumb
+  .global  Main
+
+Main:
+
+  @ Write your program here
+
+  
+
+  @ End of program ... check your result
+
+End_Main:
+  BX    lr
+
+  .end
---
+++ prog-1-sandbox/test.s
@@ -0,0 +1,18 @@
+  .syntax unified
+  .cpu cortex-m4
+  .thumb
+  .global  Init_Test
+
+  .section  .text
+
+  .type     Init_Test, %function
+Init_Test:
+  @ Initialise registers with your test vlues here
+
+
+  @ Do not edit below this line
+  bx    lr
+
+.end
+
+@ Hello Elliot!
